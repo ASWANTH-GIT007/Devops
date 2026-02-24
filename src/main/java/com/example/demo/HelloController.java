c14e22384b738589baef20a0222168e723d9e108
Aswanth <aswanth.27csa@licet.ac.in>
2026-02-24 11:07:10 +0530

    test

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World from Spring Boot!";
+    }
+
+    @Controller
+    @RequestMapping("/welcome")
+    public class WelcomeController {
+
+        @GetMapping
+        public String welcome(Model model) {
+            return "forward:/welcome.html";
+        }
+    }
+}
